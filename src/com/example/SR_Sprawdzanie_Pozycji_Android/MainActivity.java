7376c97af94b469f8febf760cd51c2b01714bb79
janwojcik5 <janwojcik5@gmail.com>
2015-09-18 21:59:51 +0200

    Tracking finished (not tested).

--- src/com/example/SR_Sprawdzanie_Pozycji_Android/MainActivity.java
+++ src/com/example/SR_Sprawdzanie_Pozycji_Android/MainActivity.java
@@ -8,16 +8,22 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.opengl.Matrix;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
+    private Sensor senMagnetic;
+    private Sensor senGravity;
 
     private long lastUpdateTimeGravity=System.currentTimeMillis();
     private long lastUpdateTimeAcceleration=System.currentTimeMillis();
+    private long lastUpdateTimeMagnetic=System.currentTimeMillis();
 
     //time in miliseconds to pass between measurements
     private final int TIME_SPAN=500;
@@ -25,6 +31,10 @@ public class MainActivity extends Activity implements SensorEventListener {
     private double positionX=0.0;
     private double positionY=0.0;
     private double positionZ=0.0;
+
+    private float[] magnetics=new float[3];
+    private float[] gravities=new float[3];
+
     /**
      * Called when the activity is first created.
      */
@@ -34,33 +44,88 @@ public class MainActivity extends Activity implements SensorEventListener {
         setContentView(R.layout.main);
 
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        senMagnetic = senSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        senGravity = senSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        senSensorManager.registerListener(this,senMagnetic,SensorManager.SENSOR_DELAY_NORMAL);
+        senSensorManager.registerListener(this,senGravity,SensorManager.SENSOR_DELAY_NORMAL);
+
+        Button button=(Button)findViewById(R.id.restartButton);
+        OnClickListener listener=new OnClickListener() {
+            private MainActivity activity;
+            public OnClickListener setActivity(MainActivity ma) {
+                activity=ma;
+                return this;
+            }
+            public void onClick(View v) {
+                activity.resetPosition();
+            }
+        }.setActivity(this);
+
+        button.setOnClickListener(listener);
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         Sensor mySensor = sensorEvent.sensor;
 
-        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+        if (mySensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            double timeDiff;
             long actualTime=System.currentTimeMillis();
             if(actualTime-lastUpdateTimeAcceleration<TIME_SPAN) {
                 return;
             } else {
+                timeDiff=(actualTime-lastUpdateTimeAcceleration)/1000.0;
                 lastUpdateTimeAcceleration=actualTime;
             }
             float x = sensorEvent.values[0];
             float y = sensorEvent.values[1];
             float z = sensorEvent.values[2];
+
+            //przeksztalcanie macierzy na wspolrzedne swiata
+            float[] r=new float[16],i=new float[16];
+
+            SensorManager.getRotationMatrix(r, i, gravities, magnetics);
+            float[] invertResult=new float[16];
+            Matrix.invertM(invertResult,0,r,0);
+
+            float[] multiplicationResult=new float[4];
+            Matrix.multiplyMV(multiplicationResult,0,invertResult,0,sensorEvent.values,0);
+
             TextView xCoordinateView=(TextView)findViewById(R.id.XCoordinate);
             TextView yCoordinateView=(TextView)findViewById(R.id.YCoordinate);
             TextView zCoordinateView=(TextView)findViewById(R.id.ZCoordinate);
 
-            xCoordinateView.setText(Float.toString(x));
-            yCoordinateView.setText(Float.toString(y));
-            zCoordinateView.setText(Float.toString(z));
+            positionX+=multiplicationResult[0]*timeDiff*timeDiff;
+            positionY+=multiplicationResult[1]*timeDiff*timeDiff;
+            positionZ+=multiplicationResult[2]*timeDiff*timeDiff;
 
-        }  else if(mySensor.getType() == Sensor.TYPE_GRAVITY) {
+            xCoordinateView.setText(Double.toString(positionX));
+            yCoordinateView.setText(Double.toString(positionY));
+            zCoordinateView.setText(Double.toString(positionZ));
+
+        }  else if(mySensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            long actualTime=System.currentTimeMillis();
+            if(actualTime-lastUpdateTimeMagnetic<TIME_SPAN) {
+                return;
+            } else {
+                lastUpdateTimeMagnetic=actualTime;
+                float x=sensorEvent.values[0];
+                float y=sensorEvent.values[1];
+                float z=sensorEvent.values[2];
+                TextView xCoordinateView=(TextView)findViewById(R.id.XCoordinate);
+                TextView yCoordinateView=(TextView)findViewById(R.id.YCoordinate);
+                TextView zCoordinateView=(TextView)findViewById(R.id.ZCoordinate);
+/*
+                xCoordinateView.setText(Float.toString(x));
+                yCoordinateView.setText(Float.toString(y));
+                zCoordinateView.setText(Float.toString(z));
+*/
+                //Button button=(Button)findViewById(R.id.restartButton);
+                //button.setTextColor(Color.BLUE);
+            }
+        } else if(mySensor.getType() == Sensor.TYPE_GRAVITY) {
             long actualTime=System.currentTimeMillis();
             if(actualTime-lastUpdateTimeGravity<TIME_SPAN) {
                 return;
@@ -72,11 +137,13 @@ public class MainActivity extends Activity implements SensorEventListener {
                 TextView xCoordinateView=(TextView)findViewById(R.id.XCoordinate);
                 TextView yCoordinateView=(TextView)findViewById(R.id.YCoordinate);
                 TextView zCoordinateView=(TextView)findViewById(R.id.ZCoordinate);
+                /*
                 xCoordinateView.setText(Float.toString(x));
                 yCoordinateView.setText(Float.toString(y));
                 zCoordinateView.setText(Float.toString(z));
                 Button button=(Button)findViewById(R.id.restartButton);
                 button.setTextColor(Color.BLUE);
+                */
             }
         }
     }
@@ -94,5 +161,22 @@ public class MainActivity extends Activity implements SensorEventListener {
     protected void onResume() {
         super.onResume();
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        senSensorManager.registerListener(this, senGravity, SensorManager.SENSOR_DELAY_NORMAL);
+        senSensorManager.registerListener(this, senMagnetic, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void resetPosition() {
+        positionX=0.0;
+        positionY=0.0;
+        positionZ=0.0;
+
+        TextView xCoordinateView=(TextView)findViewById(R.id.XCoordinate);
+        TextView yCoordinateView=(TextView)findViewById(R.id.YCoordinate);
+        TextView zCoordinateView=(TextView)findViewById(R.id.ZCoordinate);
+
+        xCoordinateView.setText(Double.toString(positionX));
+        yCoordinateView.setText(Double.toString(positionY));
+        zCoordinateView.setText(Double.toString(positionZ));
     }
 }
+
