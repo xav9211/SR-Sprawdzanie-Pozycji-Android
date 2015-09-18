6585f0e1b15c6722ef430ec970e99d947a2bac08
janwojcik5 <janwojcik5@gmail.com>
2015-09-18 19:23:07 +0200

    Next step.

--- src/com/example/SR_Sprawdzanie_Pozycji_Android/MainActivity.java
+++ src/com/example/SR_Sprawdzanie_Pozycji_Android/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.SR_Sprawdzanie_Pozycji_Android;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements SensorEventListener {
@@ -14,9 +16,15 @@ public class MainActivity extends Activity implements SensorEventListener {
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
 
-    private long lastUpdateTime=System.currentTimeMillis();
+    private long lastUpdateTimeGravity=System.currentTimeMillis();
+    private long lastUpdateTimeAcceleration=System.currentTimeMillis();
+
     //time in miliseconds to pass between measurements
     private final int TIME_SPAN=500;
+
+    private double positionX=0.0;
+    private double positionY=0.0;
+    private double positionZ=0.0;
     /**
      * Called when the activity is first created.
      */
@@ -34,27 +42,42 @@ public class MainActivity extends Activity implements SensorEventListener {
     public void onSensorChanged(SensorEvent sensorEvent) {
         Sensor mySensor = sensorEvent.sensor;
 
-        long actualTime=System.currentTimeMillis();
-        if(actualTime-lastUpdateTime<TIME_SPAN) {
-            return;
-        } else {
-            lastUpdateTime=actualTime;
-        }
-
-        if (mySensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            long actualTime=System.currentTimeMillis();
+            if(actualTime-lastUpdateTimeAcceleration<TIME_SPAN) {
+                return;
+            } else {
+                lastUpdateTimeAcceleration=actualTime;
+            }
             float x = sensorEvent.values[0];
             float y = sensorEvent.values[1];
             float z = sensorEvent.values[2];
             TextView xCoordinateView=(TextView)findViewById(R.id.XCoordinate);
             TextView yCoordinateView=(TextView)findViewById(R.id.YCoordinate);
             TextView zCoordinateView=(TextView)findViewById(R.id.ZCoordinate);
+
             xCoordinateView.setText(Float.toString(x));
             yCoordinateView.setText(Float.toString(y));
             zCoordinateView.setText(Float.toString(z));
-        } else if(mySensor.getType() == Sensor.TYPE_GYROSCOPE) {
-            float xRotation = sensorEvent.values[0];
-            float yRotation = sensorEvent.values[1];
-            float zRotation = sensorEvent.values[2];
+
+        }  else if(mySensor.getType() == Sensor.TYPE_GRAVITY) {
+            long actualTime=System.currentTimeMillis();
+            if(actualTime-lastUpdateTimeGravity<TIME_SPAN) {
+                return;
+            } else {
+                lastUpdateTimeGravity=actualTime;
+                float x=sensorEvent.values[0];
+                float y=sensorEvent.values[1];
+                float z=sensorEvent.values[2];
+                TextView xCoordinateView=(TextView)findViewById(R.id.XCoordinate);
+                TextView yCoordinateView=(TextView)findViewById(R.id.YCoordinate);
+                TextView zCoordinateView=(TextView)findViewById(R.id.ZCoordinate);
+                xCoordinateView.setText(Float.toString(x));
+                yCoordinateView.setText(Float.toString(y));
+                zCoordinateView.setText(Float.toString(z));
+                Button button=(Button)findViewById(R.id.restartButton);
+                button.setTextColor(Color.BLUE);
+            }
         }
     }
 
